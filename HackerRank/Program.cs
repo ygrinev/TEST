c2e17ba7def194340714b0441c717348e96c9741
Yuri Grinev <ygrinev@gmail.com>
2020-10-15 14:36:15 -0400

    Added TextWriter sample

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1174,6 +1174,42 @@ namespace HackerRank
             Console.WriteLine(res);
             Console.ReadKey();
         }
+
+        // ********************************************************************************************************
+        // **********************************  FOR READ WRITE OPERATIONS*******************************************
+        // ********************************************************************************************************
+        static void TextWriterCode(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string[] nq = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nq[0]);
+
+            int q = Convert.ToInt32(nq[1]);
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+            ;
+
+            int[] queries = new int[q];
+
+            for (int queriesItr = 0; queriesItr < q; queriesItr++)
+            {
+                int queriesItem = Convert.ToInt32(Console.ReadLine());
+                queries[queriesItr] = queriesItem;
+            }
+
+            int[] result = new int[] { }; // solve(arr, queries);
+
+            textWriter.WriteLine(string.Join("\n", result));
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+        // ********************************************************************************************************
+        // ********************************************************************************************************
+        // ********************************************************************************************************
+
         // Complete the hourglassSum function below.
         //static int hourglassSum(int[][] arr)
         //{
