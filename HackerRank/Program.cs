6be0672a18174c62fd02073bdd93d2fa6b30fa17
Yuri Grinev <ygrinev@gmail.com>
2020-11-05 12:10:56 -0500

    Cleanup namespaces

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,27 +1,11 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HackerRank
+﻿namespace HackerRank
 {
-    using System.CodeDom.Compiler;
+    using System;
     using System.Collections.Generic;
-    using System.Collections;
-    using System.ComponentModel;
-    using System.Diagnostics.CodeAnalysis;
-    using System.Globalization;
     using System.IO;
     using System.Linq;
-    using System.Reflection;
-    using System.Runtime.Serialization;
-    using System.Text.RegularExpressions;
     using System.Text;
-    using System;
-    using System.CodeDom;
-    using System.Runtime.InteropServices;
+    using System.Text.RegularExpressions;
 
     class Node
     {
