3c8d11ac0429e39efcb68238f5ef405db8cc95b3
Yuri Grinev <ygrinev@gmail.com>
2025-08-24 12:54:36 -0400

    Added test case

--- Test_Balance/Program.cs
+++ Test_Balance/Program.cs
@@ -3,6 +3,7 @@
 using Test_Balance;
 do
 {
-    Console.WriteLine($"Max subset length = {new TestSOTI().getMaxProdLen([1, 2, 8, 1, 8, 1, 1, 2, 2, 1, 1, 2, 7], 8)}");
+    Console.WriteLine($"Max subset length = {new TestSOTI().getMaxProdLen([1, 2, 1, 1, 1, 8, 1, 1, 2, 1, 1, 8, 1, 1, 2, 2, 1, 1, 2, 7], 8)}");
+    Console.WriteLine($"Max subset length = {new TestSOTI().getMaxProdLen([1, 2, 1, 1, 1, 8, 1, 1, 1, 1, 1, 8, 1, 1, 2, 2, 1, 1, 2, 7], 8)}");
     //Console.WriteLine($"Balance = {new BalCalculator().CalculateBalance([1,2,8,7])}");
 } while (Console.ReadKey().Key != ConsoleKey.Escape);
