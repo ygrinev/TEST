a415f89564f9196a0a810809a2879299713c38b7
Yuri Grinev <ygrinev@gmail.com>
2025-08-24 12:49:13 -0400

    added console loop

--- MongoDbAspNetCore/MongoDbAspNetCore.DataAccess/obj/Debug/netstandard2.0/MongoDbAspNetCore.DataAccess.AssemblyInfo.cs
+++ MongoDbAspNetCore/MongoDbAspNetCore.DataAccess/obj/Debug/netstandard2.0/MongoDbAspNetCore.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MongoDbAspNetCore.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e57b41ac5196015a63dc6fe38d99a5cb92d8b39")]
 [assembly: System.Reflection.AssemblyProductAttribute("MongoDbAspNetCore.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MongoDbAspNetCore.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Test_Balance/Program.cs
+++ Test_Balance/Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Test_Balance;
-
-Console.WriteLine($"Max subset length = {new TestSOTI().getMaxProdLen([1,2,8,1,8,1,1,2,2,1,1,2,7], 8)}");
-//Console.WriteLine($"Balance = {new BalCalculator().CalculateBalance([1,2,8,7])}");
-Console.ReadKey();
+do
+{
+    Console.WriteLine($"Max subset length = {new TestSOTI().getMaxProdLen([1, 2, 8, 1, 8, 1, 1, 2, 2, 1, 1, 2, 7], 8)}");
+    //Console.WriteLine($"Balance = {new BalCalculator().CalculateBalance([1,2,8,7])}");
+} while (Console.ReadKey().Key != ConsoleKey.Escape);
