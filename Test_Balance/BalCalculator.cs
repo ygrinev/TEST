7e57b41ac5196015a63dc6fe38d99a5cb92d8b39
Yuri Grinev <ygrinev@gmail.com>
2025-08-24 12:33:19 -0400

    Added 2025 test stuff

---
+++ Test_Balance/BalCalculator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Balance
+{
+	internal class BalCalculator
+	{
+		private int bal = 0;
+		public int getBalance() => bal;
+		public BalCalculator() { }
+		/// <summary>
+		/// Calculates the MAX absolute difference between adjacent sorted int array members.
+		/// Every RADIX iteration can bring the final result, so, the workflow keeps an eye on that.
+		/// </summary>
+		/// <remarks>This method uses radix/backet sort principles.</remarks>
+		/// <param name="arr">An array of integers from 0 to 2^32-1.</param>
+		/// <returns>The Max Abs difference of adjacent members of the array after sorting it.</returns>
+		public int CalculateBalance(int[] arr)
+		{
+			bal = 0;
+			if ((arr?.Length ?? 0) < 2) return 0;
+			int curBase = 1000000000;
+			List<List<int>> buckets = new List<List<int>> { arr != null ? arr.ToList() : new List<int>() };
+			while (curBase > 0)
+			{
+				buckets = getNextBucketsAndBal(curBase, buckets);
+				if (bal > 0) break;
+				curBase /= 10;
+			}
+			return bal;
+		}
+
+		private List<List<int>> getNextBucketsAndBal(int curBase, List<List<int>> buckets) =>
+			buckets.Aggregate(new List<List<int>>(), (lst, el) => getLowerBucketSetBal(curBase, lst, el));
+
+		private List<List<int>> getLowerBucketSetBal(int curBase, List<List<int>> lst, List<int> el)
+		{
+			var seed = new List<List<int>>[10].Select(el=>new List<int>()).ToList();
+			var newBackets = el.Aggregate(seed, (lst, n) => { var idx = (n / curBase) % 10; (lst[idx]??(lst[idx] = new List<int>())).Add(n); return lst; }).Where(el=>(el?.Count??0) > 0);
+			checkCurBalance(curBase, newBackets);
+			lst.AddRange(newBackets);
+
+			return lst;
+		}
+
+		private void checkCurBalance(int curBase, IEnumerable<List<int>> newBackets)
+		{
+			bool found = false;
+			int curNum = -1;
+			foreach(var ls in newBackets)
+			{
+				if (ls.Count == 1)
+				{
+					if(found) bal = Math.Max(bal, Math.Abs(ls[0] - curNum));
+					else found = true;
+					curNum = ls[0];
+				}
+				else
+					found = false;
+			}
+		}
+
+		private void test001()
+		{
+			var ll = new List<List<int>>[10];
+			var l = ll[0];
+		}
+	}
+}
---
+++ Test_Balance/TestSOTI.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Balance
+{
+	internal class TestSOTI
+	{
+		/// <summary>
+		/// Calculates the maximum length of a contiguous subarray where where product of its elements equal prod.
+		/// </summary>
+		/// <param name="arr">An array of integers to analyze.</param>
+		/// <param name="prod">Target product value.</param>
+		/// <returns>The length of the longest contiguous subarray where where product of its elements equal prod.</returns>
+		private List<int> lst = new List<int>();
+		private int targetProd = 1;
+		private int curProd = 1;
+		private bool adding = false;
+		public int getMaxProdLen(int[] arr, int prod)
+		{
+			targetProd = prod;
+			return arr?.Aggregate(0, (len, n) => getNextLen(len, n))??0;
+		}
+
+		private int getNextLen(int maxLen, int n)
+		{
+			if (adding)
+			{
+				if (isDivisor(n))
+				{
+					if(n > 1) curProd *= n;
+					lst.Add(n);
+					if(curProd <= targetProd) 
+						maxLen = checkMax(maxLen, curProd, targetProd, lst);
+					else
+					{
+						int idx = 0;
+						while(idx < lst.Count && curProd > targetProd)
+							curProd /= lst[idx++];
+                        lst = lst.Skip(idx).ToList();
+						maxLen = checkMax(maxLen, curProd, targetProd, lst);
+                    }
+				}
+				else
+				{
+					adding = false;
+					lst = new List<int>();
+					curProd = 1;
+				}
+			}
+			else
+			{
+				if(adding = isDivisor(n))
+				{
+                    lst.Add(n);
+                    curProd = n;
+                    maxLen = checkMax(maxLen, curProd, targetProd, lst);
+
+                }
+            }
+            return maxLen;
+		}
+
+		private bool isDivisor(int n) => n != 0 && (n==1 || targetProd % n == 0);
+		private int checkMax(int maxLen, int curProd, int targetProd, List<int> lst) => curProd == targetProd ? Math.Max(maxLen, lst.Count) : maxLen;
+	}
+}
